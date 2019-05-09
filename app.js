a53ff2d0d67c2da7bdfdfea01f1edd565f31aac6
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-05-08 22:21:52 -0700

    Add the index/new, index/edit, index/delete routes

--- app.js
+++ app.js
@@ -12,6 +12,26 @@ app.get("/index/json", (req, res)=>{
     apiCall.retrievePost(sendResponse);
 });
 
+// ROUTE ADD NEW POST
+app.get("/index/new", (req, res)=>{
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    res.status(200)
+    .send("THIS IS THE ADD NEW POST GET ROUTE");
+});
+
+// ROUTE EDIT POST
+app.get("/index/:id/edit", (req, res)=>{
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    res.status(200)
+    .send("THIS IS THE EDIT ROUTE");
+});
+
+app.get("/index/:id/delete", (req, res)=>{
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    res.status(200)
+    .send("THIS THE DELETE ROUTE");
+});
+
 
 
 app.listen(8000, ()=>{
